4a27e488f5a510266cf0d5f7b92be874bd57adeb
Jakub <jakub.gawron@me.com>
2018-08-11 10:02:50 +0200

    add function to reset game and action on btn

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,11 +3,15 @@
     <div class="wrapper--person">
       <div class="person--player">
         <p>You</p>
-        <div class="person--health player--health">{{healthYou}}</div>
+        <div class="person--health">
+          <span v-bind:style="{width:healthYou+'%'}">{{healthYou}}</span>
+        </div>
       </div>
       <div class="person--player">
         <p>Monster</p>
-        <div class="person--health monster--health">{{healthMonster}}</div>
+        <div class="person--health">
+          <span v-bind:style="{width:healthMonster+'%'}">{{healthMonster}}</span>
+        </div>
       </div>
     </div>
     <div class="wrapper--action">
@@ -15,13 +19,23 @@
       <button v-if="!startGame" class="btn btn-new-game" v-on:click="startNewGame">Start New Game</button>
 
       <template v-else>
-        <button class="btn btn-atack">Attack</button>
-        <button class="btn btn-extra-atack">Extra Attack</button>
-        <button class="btn btn-health">Health</button>
+        <button v-on:click="attackAction" class="btn btn-atack">Attack</button>
+        <button v-on:click="attackExtraAction" class="btn btn-extra-atack">Extra Attack</button>
+        <button v-on:click="healthAction" class="btn btn-health">Health</button>
         <button v-on:click="resetGame" class="btn btn-give-up">Give Up</button>
       </template>
 
     </div>
+
+    <div class="wrapper--list--action">
+      <ul class="action--list">
+        <li class="action--item" v-for="action in actionList" v-bind:key="action.id">
+          <p>{{action.youAction}}</p>
+          <p>{{action.monsterAction}}</p>
+        </li>
+      </ul>
+    </div>
+
     <div v-show="gameEnd" class="wrapper--info">
       <div class="wrapper--info--text">
         <p class="info--title">Game Over</p>
@@ -43,6 +57,18 @@ export default {
       startGame : false,
       msg: 'Ohhh... no you Give Up... why?',
       gameEnd: false,
+      actionList: [
+        {
+          id: 0,
+          youAction: 0,
+          monsterAction: 0
+        },
+        {
+          id: 1,
+          youAction: 0,
+          monsterAction: 0
+        }
+      ]
     }
   },
   methods: {
@@ -52,11 +78,53 @@ export default {
     resetGame: function() {
       this.startGame = !this.startGame;
       this.gameEnd = !this.gameEnd;
+
     },
     closeModal : function() {
       this.gameEnd = !this.gameEnd;
+      this.startGame = !this.startGame;
+      this.healthYou =100;
+      this.healthMonster = 100;
+    },
+    attackAction: function() {
+      this.healthYou -= this.getRandomNumner(0,10);
+      this.healthMonster -= this.getRandomNumner(0,10);
 
+      this.checkResults()
+
+    },
+    attackExtraAction: function() {
+      this.healthYou -= this.getRandomNumner(0,20);
+      this.healthMonster -= this.getRandomNumner(0,20);
+
+      this.checkResults()
+    },
+    healthAction: function() {
+
+      this.healthYou += this.getRandomNumner(0,10);
+      this.healthMonster += this.getRandomNumner(0,10);
+
+      if(this.healthYou >= 100) this.healthYou = 100;
+      if(this.healthMonster >= 100) this.healthMonster = 100;
+    },
+    getRandomNumner: function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    checkResults: function() {
+
+      if(this.healthYou <= 0) {
+        this.healthYou = 0;
+        this.gameEnd = !this.gameEnd;
+        this.msg = 'Ohhh no.... Monster kill You.... Try again'
+      }
+
+      if(this.healthMonster <= 0) {
+        this.healthMonster = 0;
+        this.gameEnd = !this.gameEnd;
+        this.msg = 'yeeeee You kill the Monster!!'
+      }
     }
+
   }
 }
 </script>
@@ -89,10 +157,16 @@ export default {
   width: 90%;
   height: 30px;
   margin: 10px auto;
-  background-color: #35BF4D;
   border: 1px solid #000;
   color: #fff;
 }
+
+.person--health span {
+  background-color: #35BF4D;
+  display: block;
+  height: 100%;
+}
+
 .wrapper--action {
   width: calc(80% + 20px);
   height: auto;
@@ -104,13 +178,41 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.wrapper--list--action {
+  width: calc(80% + 20px);
+  height: auto;
+  margin: 10px auto;
+  border: 1px solid #ccc;
+  box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
+}
+
+.action--list {
+  list-style: none;
+  padding: 0;
+  margin: 10px;
+}
+
+.action--item {
+  border: 1px solid #eee;
+  margin: 10px 0;
+  padding: 0 10px;
+}
+
+.action--list p:first-child {
+  padding: 10px;
+  background-color: #35BF4D;
+}
+.action--list p:last-child {
+  padding: 10px;
+  background-color: red;
+}
+
 .wrapper--info {
   position: absolute;
   height: 200px;
   width: 300px;
   background-color: #fff;
   box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
-
   left: 50%;
   transform: translate(-50%,-50%);
 }
@@ -124,8 +226,13 @@ export default {
   flex-direction: column;
   position: relative;
 }
+.wrapper--info--text p{
+  font-size: 18px;
+  font-weight: 700;
+}
 
 .close--element {
+  margin: 5px;
   width: 30px;
   height: 30px;
   position: absolute;
