c4dc233c232aed246692b905b373019c472a78ab
Jakub <jakub.gawron@me.com>
2018-08-11 09:03:16 +0200

    add modal and function to end game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -10,7 +10,7 @@
         <div class="person--health monster--health">{{healthMonster}}</div>
       </div>
     </div>
-    <div class="wrapper-action">
+    <div class="wrapper--action">
 
       <button v-if="!startGame" class="btn btn-new-game" v-on:click="startNewGame">Start New Game</button>
 
@@ -18,9 +18,17 @@
         <button class="btn btn-atack">Attack</button>
         <button class="btn btn-extra-atack">Extra Attack</button>
         <button class="btn btn-health">Health</button>
-        <button class="btn btn-give-up">Give Up</button>
+        <button v-on:click="resetGame" class="btn btn-give-up">Give Up</button>
       </template>
 
+    </div>
+    <div v-show="gameEnd" class="wrapper--info">
+      <div class="wrapper--info--text">
+        <p class="info--title">Game Over</p>
+        <p>{{msg}}</p>
+        <div v-on:click="closeModal" class="close--element">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,11 +41,21 @@ export default {
       healthYou : 100,
       healthMonster : 100,
       startGame : false,
+      msg: 'Ohhh... no you Give Up... why?',
+      gameEnd: false,
     }
   },
   methods: {
     startNewGame: function() {
       this.startGame = !this.startGame;
+    },
+    resetGame: function() {
+      this.startGame = !this.startGame;
+      this.gameEnd = !this.gameEnd;
+    },
+    closeModal : function() {
+      this.gameEnd = !this.gameEnd;
+
     }
   }
 }
@@ -48,6 +66,10 @@ export default {
 
 .wrapper--person {
   text-align: center;
+}
+
+.wrapper--app {
+  position: relative;
 }
 
 .person--player {
@@ -71,7 +93,7 @@ export default {
   border: 1px solid #000;
   color: #fff;
 }
-.wrapper-action {
+.wrapper--action {
   width: calc(80% + 20px);
   height: auto;
   min-height: 80px;
@@ -82,6 +104,55 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.wrapper--info {
+  position: absolute;
+  height: 200px;
+  width: 300px;
+  background-color: #fff;
+  box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.3);
+
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+.wrapper--info--text {
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+}
+
+.close--element {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+
+}
+.close--element::before,
+.close--element::after {
+  content: "";
+  height: 3px;
+  background-color: #000;
+  width: 100%;
+  display: block;
+  position: absolute;
+  top: 50%;
+  border-radius: 10px;
+}
+
+.close--element::before{
+  transform: rotate(45deg)
+}
+.close--element::after{
+  transform: rotate(-45deg)
+}
+
 .btn {
   width: 150px;
   height: 40px;
